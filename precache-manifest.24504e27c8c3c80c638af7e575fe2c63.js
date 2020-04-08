fb1eaaa04a9ee9a21f38295e34fdd90a207d0d7f
Sinien Rahma <4math2379@users.noreply.github.com>
2020-04-08 06:24:03 +0200

    Updates

---
+++ precache-manifest.24504e27c8c3c80c638af7e575fe2c63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9b2f82c670cee66da91c9414ceada7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ea4a2e522217311f6d",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "d87f5bc14bc58c147304",
+    "url": "/static/css/main.d13dffc1.chunk.css"
+  },
+  {
+    "revision": "f9ea4a2e522217311f6d",
+    "url": "/static/js/2.7a223689.chunk.js"
+  },
+  {
+    "revision": "4163c5942ed7e64ec5b69403fa56133e",
+    "url": "/static/js/2.7a223689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87f5bc14bc58c147304",
+    "url": "/static/js/main.48874605.chunk.js"
+  },
+  {
+    "revision": "6a3ba521a668b4faa410",
+    "url": "/static/js/runtime-main.248e62a3.js"
+  },
+  {
+    "revision": "c9a3f18ea696f0e0549d5df9d1764330",
+    "url": "/static/media/logoB101.c9a3f18e.png"
+  }
+]);
